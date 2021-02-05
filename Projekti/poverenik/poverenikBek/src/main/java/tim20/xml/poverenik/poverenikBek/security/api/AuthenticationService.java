0875f8d5aeb297a84344d9ac7e6ee9e8984921b9
bodroza.joca <bodroza.joca@gmail.com>
2021-02-05 14:32:17 +0100

    aaa

--- Projekti/poverenik/poverenikBek/src/main/java/tim20/xml/poverenik/poverenikBek/security/api/AuthenticationService.java
+++
@@ -1,8 +0,0 @@
-package tim20.xml.poverenik.poverenikBek.security.api;
-
-import tim20.xml.poverenik.poverenikBek.model.user.User;
-
-public interface AuthenticationService {
-    User authenticate(String email, String password);
-    User getAuthenticated();
-}
--- Projekti/poverenik/poverenikBek/src/main/java/tim20/xml/poverenik/poverenikBek/security/api/TokenService.java
+++
@@ -1,9 +0,0 @@
-package tim20.xml.poverenik.poverenikBek.security.api;
-
-import tim20.xml.poverenik.poverenikBek.model.user.User;
-
-public interface TokenService {
-    String getToken(User user);
-
-    long getExpiresIn();
-}
