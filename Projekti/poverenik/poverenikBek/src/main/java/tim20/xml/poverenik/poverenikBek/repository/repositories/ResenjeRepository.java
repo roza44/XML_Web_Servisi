ca3f95d6e9c35852d3d1f6872ce0c17afffa362d
Vukasin <vukasinj98@gmail.com>
2021-02-06 21:58:36 +0100

    Added getAll resenja for poverenik

--- Projekti/poverenik/poverenikBek/src/main/java/tim20/xml/poverenik/poverenikBek/repository/repositories/ResenjeRepository.java
+++ Projekti/poverenik/poverenikBek/src/main/java/tim20/xml/poverenik/poverenikBek/repository/repositories/ResenjeRepository.java
@@ -9,6 +9,7 @@ import tim20.xml.poverenik.poverenikBek.repository.util.DatabaseManager;
 import javax.xml.bind.JAXBException;
 import javax.xml.transform.TransformerException;
 import java.io.IOException;
+import java.util.List;
 
 @Repository
 public class ResenjeRepository {
@@ -17,7 +18,11 @@ public class ResenjeRepository {
     private final String rdfFilePath = "temp/Resenje.rdf";
 
     public void store(Resenje resenje) throws XMLDBException, JAXBException, TransformerException, IOException, SAXException {
-        DatabaseManager.<Resenje>store(resenje, COLLECTION_ID, "1.xml");
+        DatabaseManager.<Resenje>store(resenje, COLLECTION_ID, null);
         DatabaseManager.<Resenje>storeRdf(resenje, rdfFilePath);
+    }
+
+    public List<Resenje> getAll() throws XMLDBException, JAXBException {
+        return DatabaseManager.<Resenje>getAll(Resenje.class, COLLECTION_ID);
     }
 }
